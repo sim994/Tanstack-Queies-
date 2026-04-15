64ced964dbc5d5e4257a78a85d0cc5a965afd07b
sim994 <hirenpipaliya418@gmail.com>
2026-04-15 12:02:33 +0530

    Data Fetching Complete

--- React-Queries/src/components/Table.jsx
+++ React-Queries/src/components/Table.jsx
@@ -1,49 +1,69 @@
-import React from 'react'
+import React from "react";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { useSearchParams } from "react-router-dom";
 
 export function Table() {
+  const [searchParams, setSearchParams] = useSearchParams({
+    limit: 5,
+    skip: 0,
+  });
+  
+  const limit = parseInt(searchParams.get("limit")) || 5;
+  const skip = parseInt(searchParams.get("skip")) || 0;
+  
+  const {
+    isLoading,
+    isError,
+    data: users,
+  } = useQuery({
+    queryKey: ["users", limit, skip],
+    queryFn: async () =>
+      await axios.get(`https://dummyjson.com/users?limit=${limit}&skip=${skip}`).then((res) => res.data),
+  });
+
+  if (isLoading) return <div>Loading...</div>;
+  if (isError) return <div>Error occurred while fetching data.</div>;
+
   return (
     <section className="data-table-section">
-        <div className="table-wrapper">
-          <table>
-            <thead>
-              <tr>
-                <th>ID</th>
-                <th>Name</th>
-                <th>Email</th>
-                <th>Age</th>
-                <th>Phone</th>
+      <div className="table-wrapper">
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Phone</th>
+              <th>Age</th>
+              <th>Gender</th>
+              <th>Country</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users?.users?.map((user) => (
+              <tr key={user.id}>
+                <td>{user.id}</td>
+                <td>
+                  {user.firstName} {user.lastName}
+                </td>
+                <td>{user.email}</td>
+                <td>{user.phone}</td>
+                <td>{user.age}</td>
+                <td>{user.gender}</td>
+                <td>{user.address?.country}</td>
               </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>1</td>
-                <td>John Doe</td>
-                <td>john.doe@example.com</td>
-                <td>30</td>
-                <td>(555) 123-4567</td>
-              </tr>
-              <tr>
-                <td>1</td>
-                <td>John Doe</td>
-                <td>john.doe@example.com</td>
-                <td>30</td>
-                <td>(555) 123-4567</td>
-              </tr>
-              <tr>
-                <td>1</td>
-                <td>John Doe</td>
-                <td>john.doe@example.com</td>
-                <td>30</td>
-                <td>(555) 123-4567</td>
-              </tr>
-            </tbody>
-            <tfoot>
-              <tr>
-                <td colSpan="5" style={{textAlign:'center'}}>Total Records: 3</td>
-              </tr>
-            </tfoot>
-          </table>
-        </div>
+            ))}
+          </tbody>
+          <tfoot>
+            <tr>
+              <td colSpan="7" style={{ textAlign: "center" }}>
+                Total Records:{users?.users?.length} Of {users?.total}
+              </td>
+            </tr>
+          </tfoot>
+        </table>
+      </div>
     </section>
-  )
+  );
 }
